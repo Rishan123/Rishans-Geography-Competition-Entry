727a0df236598892b7b4c4760cc7306714712b01
Rishan123 <alok76in@gmail.com>
2020-10-27 18:43:53 +0000

    added wiring diagram

--- Stepper-motors/test-motors.py
+++ Stepper-motors/test-motors.py
@@ -1,6 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
 control_pins = [4,17,27,22]
 for pin in control_pins:
   GPIO.setup(pin, GPIO.OUT)
@@ -15,10 +17,10 @@ halfstep_seq = [
   [0,0,0,1],
   [1,0,0,1]
 ]
-while True:
-    for i in range(512):
-      for halfstep in range(8):
-        for pin in range(4):
-          GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
-        time.sleep(0.001)
+
+for i in range(512):
+  for halfstep in range(8):
+    for pin in range(4):
+      GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+    time.sleep(0.001)
 GPIO.cleanup()
